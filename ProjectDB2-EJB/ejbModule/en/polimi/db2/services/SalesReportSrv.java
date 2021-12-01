48bc1b6a8d39b1682cf8dcd384bb55b598faa85a
CanaliDavide <79902786+CanaliDavide@users.noreply.github.com>
2021-12-01 16:25:35 +0100

    query 2 4

--- ProjectDB2-EJB/ejbModule/en/polimi/db2/services/SalesReportSrv.java
+++ ProjectDB2-EJB/ejbModule/en/polimi/db2/services/SalesReportSrv.java
@@ -31,8 +31,8 @@ public class SalesReportSrv {
 	@SuppressWarnings("unchecked")
 	public List<Object[]> totalPurchasePerPerckageAndValidity() {
 		Query query = em.createQuery(
-				"select o.idPack, o.namePack, o.numPurc, vp.month"
-				+ " from PurchasesPackageValidity o join Validityperiod vp on o.idValidity = vp.id");
+				"select o.id.idPack , o.namePack, o.numPurc, vp.month"
+				+ " from PurchasesPackageValidity o join Validityperiod vp on o.id.idValidity = vp.id");
 		return query.getResultList();
 	}
 	
